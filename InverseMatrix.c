785152edf4b8e702296dc43149a768ece4a0a1df
Fazil Raja <fazilraja13@gmail.com>
2021-10-05 22:46:10 -0500

    Cleaned up the output and added if condition to check if determinant is 0

--- InverseMatrix.c
+++ InverseMatrix.c
@@ -5,7 +5,8 @@ int main() {
     int det = 0;//determinant
 
     printf("Enter elements of a matrix row wise: \n");
-    //gets the user input for the matrix
+
+     //gets the user input for the matrix
     for(int i = 0; i < 2; i++) {
         for(int j = 0; j < 2; j++) {
             scanf("%d", &mat[i][j]); 
@@ -20,6 +21,7 @@ int main() {
             printf("%d\t", mat[i][j]);
         }
     }
+    printf("\n");
 
     //find the determinant
     //ad - bc
@@ -27,19 +29,25 @@ int main() {
 
     printf("The determinant is: %d\n", det);
 
-    //switch the matrix around to solve for inverse
-    int temp = mat[0][0];
-    mat[0][0] = mat[1][1];
-    mat[1][1] = temp;
-    mat[0][1] = - mat[0][1];
-    mat[1][0] = - mat[1][0];
+    //determinant cannot be 0
+    if(det != 0) {
+        //switch the matrix around to solve for inverse
+        int temp = mat[0][0];
+        mat[0][0] = mat[1][1];
+        mat[1][1] = temp;
+        mat[0][1] = - mat[0][1];
+        mat[1][0] = - mat[1][0];
 
-    printf("The inverse of matrix is: \n");
-    
-    for(int i = 0; i < 2; i++) {
-        for(int j = 0; j < 2; j++) {
-            printf("%d\t", mat[i][j]/det);
-            printf("\n"); 
+        printf("The inverse of matrix is:");
+
+        //prints the inverse back to the user
+        for(int i = 0; i < 2; i++) {
+            printf("\n");
+            for(int j = 0; j < 2; j++) {
+                printf("%d\t", mat[i][j]/det); 
+            }
         }
     }
+
+
 }
